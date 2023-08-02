fe722fc0edce0a84dbc8c4c6cc5aa418c0ab3083
Dilan-Biou <mr.dilan.biou.0099@gmail.com>
2023-08-02 15:09:30 +0330

    Deploy Websit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ export default function Navbar(){
     return(
         <div className="nav-bar">
             <h1 className="logo-name">Uni<span>Qar</span></h1>
-            <img className="question-mark" src="./public/question-mark.png"/>
         </div>
     )
 }
